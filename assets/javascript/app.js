36a9a76189fcf7c7493115c589678a0895f3491c
Brian <rac3k@aol.com>
2018-06-09 21:18:42 -0400

    var add and classes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+var questionArr = ['What is Javascript?', 'What is NaN?'];
+var answersArr = [['Programming Language', 'Type of Coffee'], ['Not-A-Number', 'Grandmother']];
 
 
-
-
-
-    
+function write() {
+    game =
+    $(".text").html(game);
+};
+write();
 });

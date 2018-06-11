fabc5f3802254282c1504bf0a7826e3b692d2a8b
Brian <rac3k@aol.com>
2018-06-10 22:05:38 -0400

    start function with questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,31 @@
 $(document).ready(function() {
 
-var questionArr = ['What is Javascript?', 'What is NaN?'];
+var questionsArr = ['What is Javascript?', 'What is NaN?'];
 var answersArr = [['Programming Language', 'Type of Coffee'], ['Not-A-Number', 'Grandmother']];
+var correctAnswers = ['A. Programming Language', 'A. Not-A-Number'];
+var counter = 0;
 
 
-function write() {
-    game =
-    $(".text").html(game);
+
+function questionChange() {
+    if (counter < 2) {
+        counter++;
+        newQuestion();
+    }
 };
-write();
+
+function newQuestion() {
+    addQuestion = "<p class='question'>" + questionsArr[counter] + "</p>" + "<p class='choices'>" +'A. ' + answersArr[counter][0] + "</p>" + "<p>" + 'B. ' + answersArr[counter][1] + "</p>";
+    $(".question").html(addQuestion);
+};
+
+$(".question").on("click", function() {
+    newQuestion();
+    setTimeout(questionChange, 5000);
+});
+
+
+
+
+
 });

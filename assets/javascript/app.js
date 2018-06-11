5af33dd50bbf272b8e30e1a46214fc3720b3f8a7
Brian <rac3k@aol.com>
2018-06-10 22:35:00 -0400

    Timer added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,17 @@ var questionsArr = ['What is Javascript?', 'What is NaN?'];
 var answersArr = [['Programming Language', 'Type of Coffee'], ['Not-A-Number', 'Grandmother']];
 var correctAnswers = ['A. Programming Language', 'A. Not-A-Number'];
 var counter = 0;
+var timeLeft = 3;
+var timerId = setInterval(countdown, 1000);
 
+function countdown() {
+    if (timeLeft == 0) {
+        clearTimeout(timerId);
+        questionChange();
+    }else {
+        timeLeft--;
+    }
+}
 
 
 function questionChange() {
@@ -13,15 +23,22 @@ function questionChange() {
         newQuestion();
     }
 };
-
+// New questions are added to the html using function;
 function newQuestion() {
-    addQuestion = "<p class='question'>" + questionsArr[counter] + "</p>" + "<p class='choices'>" +'A. ' + answersArr[counter][0] + "</p>" + "<p>" + 'B. ' + answersArr[counter][1] + "</p>";
+//question are changed over counter function;
+    addQuestion = "<p class='question'>" + questionsArr[counter] + "</p>" + 
+//muiltiple choices are charged over counter function as well;
+        "<p class='choices'><p id=choiceA'> " +'A. ' + answersArr[counter][0] + "</p>" + 
+        "<p id='choiceB'>" + 'B. ' + answersArr[counter][1] + "</p>";
+
+
     $(".question").html(addQuestion);
 };
 
 $(".question").on("click", function() {
     newQuestion();
-    setTimeout(questionChange, 5000);
+    countdown();
+    // setTimeout(questionChange, 50000000);
 });
 
 

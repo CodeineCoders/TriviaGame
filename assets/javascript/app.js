36c19a8bbee2b353ac72431ae0044927a4677bba
Brian <rac3k@aol.com>
2018-06-11 22:29:50 -0400

    added correct answer function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 
 var questionsArr = ['What is Javascript?', 'What is NaN?'];
 var answersArr = [['Programming Language', 'Type of Coffee'], ['Not-A-Number', 'Grandmother']];
-var correctAnswers = ['A. Programming Language', 'A. Not-A-Number'];
+var correctAnswers = ['Programming Language', 'Not-A-Number'];
 var counter = 0;
-var timeLeft = 3;
+var timeLeft = 30;
 var timerId = setInterval(countdown, 1000);
 
 function countdown() {
@@ -26,20 +26,22 @@ function questionChange() {
 // New questions are added to the html using function;
 function newQuestion() {
 //question are changed over counter function;
-    addQuestion = "<p class='question'>" + questionsArr[counter] + "</p>" + 
-//muiltiple choices are charged over counter function as well;
-        "<p class='choices'><p id=choiceA'> " +'A. ' + answersArr[counter][0] + "</p>" + 
-        "<p id='choiceB'>" + 'B. ' + answersArr[counter][1] + "</p>";
-
-
-    $(".question").html(addQuestion);
+    addQuestion = "<p class='question'>" + questionsArr[counter] + "</p><p class='question'><p class='answer'>A. " + answersArr[counter][0] + "</p><p class='answer'>B. " + answersArr[counter][1] + "</p>";
+        $(".question").html(addQuestion);
+    
 };
 
+
+// Start Game;
 $(".question").on("click", function() {
     newQuestion();
     countdown();
-    // setTimeout(questionChange, 50000000);
+
 });
+
+$(".question").on("click", ".answer", function() {
+    alert("Correct");
+})
 
 
 
